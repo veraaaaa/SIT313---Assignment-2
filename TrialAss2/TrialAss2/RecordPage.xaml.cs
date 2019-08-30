d6ef4ea0c6cb3e33e0a84109a0dfe9ead6e9d3eb
王雨薇 <vera@10-140-31-59.wifi-m.deakin.edu.au>
2019-08-30 13:47:25 +1000

    update

---
+++ TrialAss2/TrialAss2/RecordPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamForms.Controls;
+
+namespace TrialAss2
+{
+    [DesignTimeVisible(false)]
+    public partial class RecordPage : ContentPage
+    {
+        public RecordPage()
+        {
+            InitializeComponent();
+            var Dates = new List<SpecialDate>();
+            NavigationPage.SetHasNavigationBar(this, false);
+            DateTime testdate = new DateTime(2018, 06, 26);
+        }
+    }
+}
