5cc915109b7df87584a958763e7da511e20ef712
Junjie Gong <kingsley__kung@hotmail.com>
2019-09-03 19:08:13 +1000

    123

---
+++ TrialAss2/TrialAss2.Android/obj/Debug/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 717c880dce715d95675d888c33ec90ca875fb9fe */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1491
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 390643
+	.include	"typemap.mj.inc"
