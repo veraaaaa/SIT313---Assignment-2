9675c8439266d1a1b4da342d38f7383587cb7c67
Junjie Gong <kingsley__kung@hotmail.com>
2019-09-21 16:30:40 +1000

    21.09 Saturday

---
+++ Kingsley(21:09)/TrialAss2/TrialAss2/ListItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using XamForms.Controls;
+
+namespace TrialAss2
+{
+    public class ListItem
+    {
+        public string ItemName { get; set; }
+        public string ItemImage { get; set; }
+        public string CheckboxImage { get; set; }
+    }
+    public class WeeklyDates
+    {
+        public DateTime Today { get; set; }
+        public DateTime SecondDay { get; set; }
+        public DateTime ThirdDay { get; set; }
+        public DateTime FourthDay { get; set; }
+        public DateTime FifthDay { get; set; }
+        public DateTime SixthDay { get; set; }
+        public DateTime Sevenday { get; set; }
+    }
+    public class CustomerInfo
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string Age { get; set; }
+        public string CycleDay { get; set; }
+        public string MenstrualDay { get; set; }
+        public DateTime LmenstrualDay { get; set; }
+    }
+    public class RecordDetails
+    {
+        public bool sex { get; set; }
+        public bool dysmenorrha { get; set; }
+        public bool headache { get; set; }
+        public bool dizziness { get; set; }
+        public DateTime SpecificDate { get; set; }
+    }
+}
