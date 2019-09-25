af1f10ffba431023a3c6a51537b20125419c0500
王雨薇 <vera@10-140-41-42.wifi-m.deakin.edu.au>
2019-09-25 14:19:01 +1000

    Update

---
+++ History Record/Assignment 2_Final Version/Assignment2/Assignment2.Android/obj/Debug/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 751c889829a1371b04c4008cd81049d34be00b10 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1357
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 355535
+	.include	"typemap.jm.inc"
---
+++ History Record/Assignment 2_Final Version/Assignment2/Assignment2/AccountSettingPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Assignment2
+{
+    public partial class AccountSettingPage : ContentPage
+    {
+        List<CustomerInfo> Logout = LoginPage.LoginList;
+        CustomerInfo logout = RegisterPage.NewCustomer;
+
+        public AccountSettingPage()
+        {
+            InitializeComponent();
+
+            var email = new Label
+            {
+                Text = "Hello ",
+                FontSize = 20,
+                TextColor = Color.Black,
+            };
+            var email2 = new Label
+            {
+                FontSize = 20,
+                TextColor = Color.Black,
+            };
+            email2.Text = logout.email;
+
+            var LogoutBtn = new Button
+            {
+                Text = "Logout",
+                TextColor = Color.Black,
+                BorderColor = Color.White,
+            };
+            LogoutBtn.Clicked += logoutbtn_clicked;
+
+            async void logoutbtn_clicked(object sender, EventArgs e)
+            {
+                await Navigation.PushAsync(new LoginPage());
+            }
+
+            Content = new StackLayout
+            {
+                
+                Children =
+                {
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new StackLayout
+                            {
+                                Children =
+                                {
+                                    email,
+                                }
+                            },
+                            new StackLayout
+                            {
+                                Children =
+                                {
+                                    email2,
+                                }
+                            },
+                        }
+                    },
+                    new StackLayout
+                    {
+                        Children =
+                        {
+                            LogoutBtn,
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
